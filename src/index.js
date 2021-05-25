6e9073c273ac8d00fa98d36082d70675119e78c2
waleed-techverx <waleed.mazhar@techverx.com>
2021-05-25 15:03:26 +0500

    Version 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const app = require('./app')
-const port = process.env.PORT
+const port = process.env.PORT  || 3000
+const path = require('path')
+const express = require('express')
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(espress.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, ()=>{
     console.log(`Server is running on https://localhost:${port}`)
